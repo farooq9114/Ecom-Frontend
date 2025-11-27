872bda1f4a9d3dc61b4ed180dc5f7a2f301a28b6
farooq9114 <syedfarooqsyedfarooq@6963@gmail.com>
2025-11-28 02:01:49 +0530

    Modified app.routes.server.ts file for prerendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,29 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Prerender only static pages
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Prerender
+  },
+
+  // ❌ Disable prerendering for dynamic pages
+  {
+    path: 'perfume/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'combos/:id',
+    renderMode: RenderMode.Server
+  },
+
+  // fallback
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
